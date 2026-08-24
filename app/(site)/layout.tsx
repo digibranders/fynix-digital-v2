0d098a7eefd2e6165d6cd5d6167cde4e13052477
Gaurav Jadhav <gaurav.vjadhav01@gmail.com>
2026-08-24 09:56:38 +0530

    Remove the site-wide masterclass announcement bar

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import SmoothScroll from "@/components/SmoothScroll";
-import AnnouncementBanner from "@/components/AnnouncementBanner";
 
 export default function SiteLayout({
   children,
@@ -17,7 +16,6 @@ export default function SiteLayout({
         Skip to main content
       </a>
       <SmoothScroll>
-        <AnnouncementBanner />
         <Header />
         <main id="main" className="flex-1 flex flex-col">
           {children}
